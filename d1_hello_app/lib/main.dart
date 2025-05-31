f555dfcdffa0ea1f2e2cd4e6c06cae823a665d8d
punit169 <punitshah387@gmail.com>
2025-05-31 15:26:56 +0530

    add d3 project

--- d1_hello_app/lib/main.dart
+++ d1_hello_app/lib/main.dart
@@ -30,6 +30,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,21 +45,30 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Name: Punit', style: TextStyle(fontSize: 20)),
-            SizedBox(height: 10),
-            Text('Role: Flutter Intern', style: TextStyle(fontSize: 18)),
-            SizedBox(height: 20),
-            Text(
+            const Text('Name: Punit', style: TextStyle(fontSize: 20)),
+            const SizedBox(height: 10),
+            const Text('Role: Flutter Intern', style: TextStyle(fontSize: 18)),
+            const SizedBox(height: 20),
+            const Text(
               '"Believe in your dreams."',
               style: TextStyle(fontStyle: FontStyle.italic, fontSize: 16),
               textAlign: TextAlign.center,
             ),
+            const SizedBox(height: 30),
+            const Text('You have clicked this button:'),
+            Text('$_counter times',
+                style: Theme.of(context).textTheme.headlineMedium),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
